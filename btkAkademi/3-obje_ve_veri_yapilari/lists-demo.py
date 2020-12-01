48d06d99f9e8ff2cfa293fc00b4182ae37c9ec3b
Keett <32227641+Keett@users.noreply.github.com>
2020-12-01 20:59:03 +0300

    Update lists-demo.py

--- btkAkademi/3-obje_ve_veri_yapilari/lists-demo.py
+++ btkAkademi/3-obje_ve_veri_yapilari/lists-demo.py
@@ -21,7 +21,7 @@ result = arabalar[-2:]
 arabalar[-2:] = ['Toyota','Renault']
 result = arabalar
 # 9-  Listenin üzerine "Audi" ve "Nissan" değerlerini ekleyin.
-result = arabalar + ['Audi','Nissan']
+arabalar = arabalar + ['Audi','Nissan']
 # 10- Listenin son elemanını silin.
 del arabalar[-1]
 result = arabalar
@@ -38,11 +38,10 @@ studentB = ['Sena','Turan',1999,[80,80,70]]
 studentC = ['Ahmet','Turan',1998,[80,70,90]]
 
 # 13- Liste elemanlarını ekrana yazdırınız.
-
 result = studentA[0]
 result = studentB[1]
 result = studentC[3][1]
 
-result = f"{studentA[0]} {studentA[1]} {2019-studentA[2]} yaşında ve not ortalaması {(studentA[3][0] + studentA[3][1] + studentA[3][2])/3}"
+result = f"{studentA[0]} {studentA[1]} {2019-studentA[2]} yaşında ve not ortalaması {int((studentA[3][0] + studentA[3][1] + studentA[3][2])/3)}"
 
 print(result)
